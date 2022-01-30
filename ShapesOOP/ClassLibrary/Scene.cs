c15cb45adf70f3da05fc9d82ebbfb37ac26b046d
Martin Terziev <48216671+mterziev@users.noreply.github.com>
2022-01-30 16:00:08 +0200

    Add files via upload

---
+++ ShapesOOP/ClassLibrary/Scene.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ShapesOOP
+{
+    [Serializable]
+    class Scene
+    {
+        private List<Shape> _shapes = new List<Shape>();
+        private List<Shape> _selectedshapes = new List<Shape>();
+
+        public void AddShape(Shape shape)
+        {
+            _shapes.Add(shape);
+        }
+
+        public void RemoveShape()
+        {
+            foreach (var shape in _selectedshapes)
+            {
+                _shapes.Remove(shape);
+
+
+            }
+
+        }
+        public void ChangeShape(Color color, int width, int height)
+        {
+            foreach (var shape in _selectedshapes)
+            {
+
+                shape.Color = color;
+                shape.Width = width;
+                shape.Height = height;
+
+            }
+        }
+
+        public void AddSelectedShape(Point location)
+        {
+            _selectedshapes = _shapes
+                        .Where(r => r.Position.X < location.X &&
+                                    r.Position.Y < location.Y &&
+                                   (r.Position.X + r.Width) > location.X &&
+                                  (r.Position.Y + r.Height) > location.Y
+                        )
+                        .ToList();
+
+        }
+        public void ShapeInfo()
+        {
+            foreach (var shape in _selectedshapes)
+            {
+                //FormShapeInfo formshapeinfo = new FormShapeInfo();
+                formshapeinfo.labelSize.Text += "(" + Convert.ToString(shape.Width) + "," + Convert.ToString(shape.Height) + ")";
+                formshapeinfo.labelPosition.Text += "(" + Convert.ToString(shape.Position.X) + "," + Convert.ToString(shape.Position.Y) + ")";
+                formshapeinfo.labelArea.Text += Convert.ToString(shape.CalculateArea());
+                
+                
+
+
+                formshapeinfo.Show();
+
+            }
+        }
+
+        public void ShapePosition(Point position)
+        {
+            foreach (var shape in _selectedshapes)
+            {
+                shape.Position = position;
+            }
+        }
+
+       // public void Paint(Graphics graphics)
+        {
+        //    foreach (var shape in _shapes)
+         //       shape.Paint(graphics, shape.Color);
+        }
+
+
+
+    }
+}
---
+++ ShapesOOP/ClassLibrary/Shapes.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ShapesOOP
+{
+    [Serializable]
+    public abstract class Shape 
+
+    {
+       
+        public Point Position { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Color Color { get; set; }
+
+        
+
+        public abstract double CalculateArea();
+
+        public abstract void Paint(Graphics graphics, Color color);
+        
+
+    }
+
+    [Serializable]
+    public class Rectangle : Shape
+    {
+        public Rectangle(int width, int height)
+        {
+            
+            this.Width = width;
+            this.Height = height;
+        }
+        
+
+
+        public override void Paint(Graphics graphics, Color color)
+        {
+
+            using (var brush = new SolidBrush(color))
+            using (var pen = new Pen(brush, 3))
+
+
+                graphics.DrawRectangle(pen, Position.X, Position.Y, Width, Height);
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+    }
+
+
+    [Serializable]
+    public class Ellipse : Shape
+    {
+        public Ellipse(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+
+
+
+
+        public override void Paint(Graphics graphics, Color color)
+        {
+
+            using (var brush = new SolidBrush(color))
+            using (var pen = new Pen(brush, 3))
+
+
+                graphics.DrawEllipse(pen, Position.X, Position.Y, Width, Height);
+        }
+
+        public override double CalculateArea()
+        {
+            return Width / 2.0 * Height / 2.0 * Math.PI;
+        }
+
+    }
+
+    [Serializable]
+    public class Triangle : Shape
+    {
+            public Triangle (int width, int height)
+            {
+                this.Width = width;
+                this.Height = height;
+            }
+
+
+
+
+
+            public override void Paint(Graphics graphics, Color color)
+            {
+
+              using (var brush = new SolidBrush(color))
+              using (var pen = new Pen(brush, 3))
+              {
+
+                graphics.DrawLine(pen, Position.X + (Width / 2), Position.Y, Position.X + Width, Position.Y + Height);
+                graphics.DrawLine(pen, Position.X + (Width / 2), Position.Y, Position.X, Position.Y + Height);
+                graphics.DrawLine(pen, Position.X, Position.Y+Height, Position.X + Width, Position.Y + Height);
+
+              }
+            }
+
+            public override double CalculateArea()
+            {
+                return (Width * Height) /2.0;
+            }
+            
+    }
+
+
+    
+}
